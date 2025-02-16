1dd73c2b4b3291f00743092934f3e28aab79757e
James Burch <burchjames6@gmail.com>
2025-02-16 12:57:23 +0000

    Add boxplot/histrogram and complete project hypothesis page

--- app_pages/page_hypothesis.py
+++ app_pages/page_hypothesis.py
@@ -10,9 +10,19 @@ def page_hypothesis_body():
 
     # conclusions taken from HouseSalePrices notebook 
     st.success(
-        f"Test"
+        f"*Suspect that there is few high sales prices in the dataset.  \n"
+        f"*The boxplot and histogram below confirms that the histogram extends to the right.  \n"
+        f"*The sales price values beyond the average range are called outliers and shown as dots.  \n"
+        f"*These dots correspond to sales prices above £466,075.  \n"
     )     
+
+    # Add boxplot and histogram to show the sale price distribution
+    df2=df.filter(['SalePrice'])
+    plot_histogram_and_boxplot(df2) 
    
     st.info(
-        f"Test"
+        f"*The models created may not be able to predict house prices accurately or at all for values above £400,000.  \n"
+        f"*This will be shown in scatterplots on the ML model page.  \n"
+        f"*This could be connected to the outliers mentioned above (with sale prices above £466,075),  \n"
+        f"We took some steps to improve the model for predicting higher prices, the sale price variable was transformed to make the distribution more symmetrical but more work will be needed.  \n"
     )
